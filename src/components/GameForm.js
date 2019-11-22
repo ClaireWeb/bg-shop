300bd026af57b8564781ee0680f34fb22825b16d
Claire <clairepelletier.prod@gmail.com>
2019-11-22 14:20:39 +0100

    Controlled form

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class GameForm extends Component {
-  state = {};
+  state = {
+    name: ''
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  handleNameChange = e => this.setState({ name: e.target.value });
+
   render() {
     return (
-      <form className="ui form">
+      <form className="ui form" onSubmit={this.handleSubmit}>
         <div className="field">
           <label htmlFor="name">Game Title</label>
-          <input type="text" id="name" placeholder="Full game title" />
+          <input
+            type="text"
+            id="name"
+            placeholder="Full game title"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
         </div>
         <button className="ui button" type="submit">
           Create
