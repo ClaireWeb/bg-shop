8c5fc7f8b94982042325c11f1154fa6388bca058
Claire <clairepelletier.prod@gmail.com>
2019-11-26 11:05:03 +0100

    Radio buttons

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -6,6 +6,12 @@ const tags = [
   { _id: 3, name: 'family' }
 ];
 
+const genres = [
+  { _id: 1, name: 'abstract' },
+  { _id: 2, name: 'euro' },
+  { _id: 3, name: 'ameritrash' }
+];
+
 class GameForm extends Component {
   state = {
     name: '',
@@ -14,7 +20,8 @@ class GameForm extends Component {
     duration: 0,
     players: '',
     featured: false,
-    tags: []
+    tags: [],
+    genre: 1
   };
 
   handleSubmit = e => {
@@ -37,6 +44,8 @@ class GameForm extends Component {
     this.state.tags.includes(tag._id)
       ? this.setState({ tags: this.state.tags.filter(id => id !== tag._id) })
       : this.setState({ tags: [...this.state.tags, tag._id] });
+
+  handleGenreChange = genre => this.setState({ genre: genre._id });
 
   render() {
     const { name, description, price, duration, players } = this.state;
@@ -118,6 +127,21 @@ class GameForm extends Component {
                 onChange={() => this.toggleTag(tag)}
               />
               <label htmlFor={`tag-${tag._id}`}>{tag.name}</label>
+            </div>
+          ))}
+        </div>
+
+        <div className="field">
+          <label>Genres</label>
+          {genres.map(genre => (
+            <div key={genre._id} className="inline field">
+              <input
+                id={`genre-${genre._id}`}
+                type="radio"
+                checked={this.state.genre === genre._id}
+                onChange={() => this.handleGenreChange(genre)}
+              />
+              <label htmlFor={`genre-${genre._id}`}>{genre.name}</label>
             </div>
           ))}
         </div>
